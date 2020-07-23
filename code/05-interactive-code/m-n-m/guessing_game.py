f8e82c02a80754855b5afb05a5aba4ff1c162220
ehbnr <33774925+ehbnr@users.noreply.github.com>
2020-07-23 20:37:40 +0000

    Add files via upload

--- code/05-interactive-code/m-n-m/guessing_game.py
+++ code/05-interactive-code/m-n-m/guessing_game.py
@@ -12,14 +12,14 @@ mm_count = random.randint(1, 100)  # Calls for a random integer between 1 and 10
 attempt_limit = 10
 attempts = 0
 
-while attempts < attempt_limit:
-    guess_text = input("How many M&Ms are in the jar? ")
-    guess = int(guess_text)
-    attempts += 1
+while attempts < attempt_limit:  # While loop to give user a certain number of attempts.
+    guess_text = input("How many M&Ms are in the jar? ")  # reminder: this value is a string
+    guess = int(guess_text)  # turns the string above into an integer
+    attempts += 1  # keeps track of number of attempts user has made
 
     if mm_count == guess:
         print(f"Winner! You got free lunch! The number was {guess}. ")
-        break
+        break  # break disrupts the loop
     elif mm_count < guess:
         print("That's too high.")
     else:
