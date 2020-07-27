b8008bf5f67b1394f1573efbc6ed3c4c1f6c057b
ehbnr <ba.cua755@protonmail.com>
2020-07-27 00:21:18 -0500

    completed rps exercise

--- code/06-organizing-code-with-functions/demo-start-rps/rps-game-1player.py
+++ code/06-organizing-code-with-functions/demo-start-rps/rps-game-1player.py
@@ -18,22 +18,49 @@ def show_header():
 
 
 def play_game(player1, player2):
+    rounds = 3
+    wins_p1 = 0
+    wins_p2 = 0
+
     rolls = ["rock", "paper", "scissors"]
 
-    roll1 = get_roll(player1, rolls)
-    roll2 = random.choice(rolls)
+    while wins_p1 < rounds and wins_p2 < rounds:
 
-    if not roll1:
-        print("Can't play that. Exiting...")
-        return
+        roll1 = get_roll(player1, rolls)
+        roll2 = random.choice(rolls)
 
-    print(f"{player1} rolls {roll1}.")
-    print(f"{player2} rolls {roll2}.")
+        if not roll1:
+            print("Try again!")
+            continue
 
+        print(f"{player1} rolls {roll1}.")
+        print(f"{player2} rolls {roll2}.")
+
+        winner = win_conditions(player1, player2, roll1, roll2)
+
+        if winner is None:
+            print("This round was a tie!")
+        else:
+            print(f"{winner} takes the round!")
+            if winner == player1:
+                wins_p1 += 1
+            elif winner == player2:
+                wins_p2 += 1
+
+        print(f"{player1}: {wins_p1} / {player2}: {wins_p2}")
+        print()
+
+    if wins_p1 >= rounds:
+        overall_winner = player1
+    else:
+        overall_winner = player2
+
+    print(f"{overall_winner} wins the game!")
+
+
+def win_conditions(player1, player2, roll1, roll2):
     # Win conditions
-
     winner = None
-
     if roll1 == roll2:
         winner = None
     elif roll1 == 'rock':
@@ -51,22 +78,22 @@ def play_game(player1, player2):
             winner = player1
         elif roll2 == 'rock':
             winner = player2
-
-    print("The game is over!")
-
-    if winner is None:
-        print("It was a tie!")
-    else:
-        print(f"{winner} takes the game!")
+    return winner
 
 
 def get_roll(player_name, rolls):
-    roll = input(f"{player_name}, enter your roll [rock, paper, scissors]: ")
-    roll = roll.lower().strip()
-    if roll not in rolls:
-        print(f"Sorry {player_name}, {roll} is not a valid roll.")
+    print("Available rolls:")
+    for index, r in enumerate(rolls, start=1):
+        print(f"{index}. {r}")
+    print()
+    text = input(f"{player_name}, what is your roll: ")
+    selected_index = int(text) - 1
+
+    if selected_index < 0 or selected_index >= len(rolls):
+        print(f"Sorry {player_name}, {text} is not a valid roll.")
         return None
-    return roll
+
+    return rolls[selected_index]
 
 
 if __name__ == '__main__':
